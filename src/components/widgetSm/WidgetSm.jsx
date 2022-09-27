ec9f522094f587214f9cd91ef1649325cd8ec98a
Yedarm Park <yedarmpark79@gmail.com>
2022-09-26 22:38:41 -0700

    first commit

---
+++ src/components/widgetSm/WidgetSm.jsx
@@ -0,0 +1,44 @@
+import "./widgetSm.css";
+import { Visibility } from "@material-ui/icons";
+import { useState, useEffect } from 'react';
+import { userRequest } from "../../requestMethods";
+
+export default function WidgetSm() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const res = await userRequest.get('users/?new=true');
+        setUsers(res.data);
+      } catch { }
+    };
+
+    getUsers();
+  }, []);
+
+  return (
+    <div className="widgetSm">
+      <span className="widgetSmTitle">New Join Members</span>
+      <ul className="widgetSmList">
+        {users.map((user) => (
+          <li className="widgetSmListItem" key={user._id}>
+            <img
+              src={user.img || 'https://grandimageinc.com/wp-content/uploads/2015/09/icon-user-default.png'}
+              alt=""
+              className="widgetSmImg"
+            />
+            <div className="widgetSmUser">
+              <span className="widgetSmUsername">{user.username}</span>
+              {/* <span className="widgetSmUserTitle">Software Engineer</span> */}
+            </div>
+            <button className="widgetSmButton">
+              <Visibility className="widgetSmIcon" />
+              Display
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
